8bb87c6735196a38705aaa4fbff093835c8baf32
Saksham Khare <sakshamkhare19@gmail.com>
2024-10-14 16:16:18 +0530

    endpoints of app - To create a new user with userID - To create a symbol in orderbook - To get orderbook variable - To get balance in inr - To get stock balance

--- index.js
+++ index.js
@@ -1,10 +1,58 @@
 import express from "express";
+import env from "dotenv";
+import { INR_BALANCES, ORDERBOOK, STOCK_BALANCES } from "./state.js";
 
 const app = express();
-const port = process.env.PORT;
+env.config();
+const port = process.env.PORT || 3000;
+
+console.log(port);
+console.log(INR_BALANCES, ORDERBOOK, STOCK_BALANCES);
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   console.log("Landing page");
+});
+
+app.post("/user/create/:userId", (req, res) => {
+  const userId = req.params.userId;
+
+  if (INR_BALANCES[userId]) {
+    return res.status(403).json({
+      message: "User Already exists",
+    });
+  }
+
+  INR_BALANCES[userId] = {
+    balance: 0,
+    locked: 0,
+  };
+
+  return res.status(200).json({
+    message: `Successfully created a user of id: ${userId}`,
+    INR_BALANCES,
+  });
+});
+
+app.post("/user/create/:stockSymbol", (req, res) => {});
+
+app.get("/orderbook", (req, res) => {
+  return res.status(200).json({
+    ORDERBOOK,
+  });
+});
+
+app.get("/balance/inr", (req, res) => {
+  return res.status(200).json({
+    INR_BALANCES,
+  });
+});
+
+app.get("/balances/stock", (req, res) => {
+  return res.status(200).json({
+    STOCK_BALANCES,
+  });
 });
 
 app.listen(port, () => {
