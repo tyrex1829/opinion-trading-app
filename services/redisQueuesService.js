b22d469aaa52c90d357a2d562dd39ead98f378a5
Saksham Khare <sakshamkhare19@gmail.com>
2024-10-27 17:03:53 +0530

    i uuid for request id

--- services/redisQueuesService.js
+++
@@ -1,9 +0,0 @@
-import client from "../queue/redisClient.js";
-
-export const addToOrderQueue = async (orderData) => {
-  await client.lPush("orderQueue", JSON.stringify(orderData));
-};
-
-export const addToCompletedQueue = async (resultData) => {
-  await client.lPush("completedTasksQueue", JSON.stringify(resultData));
-};
